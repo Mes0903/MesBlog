4e55bc867fd94b77f8599c106ab516d6268c85c6
Mes <mes900903@gmail.com>
2025-02-25 22:14:16 +0800

    Update style

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,87 +3,57 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar([
   // "/",
   // {
-  //   text: "不分類筆記",
+  //   text: "雜項",
   //   children: [
   //     {
   //       text: "網路",
   //       prefix: "/computer_network/",
-  //       children: [
-  //         "GSM_UMTS_IGP/"
-  //       ],
+  //       children: "structure",
   //     },
   //     {
   //       text: "memory",
   //       prefix: "/memory/",
-  //       children: [
-  //         "sw-reram",
-  //       ],
+  //       children: "structure",
   //     },
   //     {
-  //       text: "雜記",
-  //       prefix: "/miscellaneous/",
-  //       children: [
-  //         "The_mind_behind_Linux"
-  //       ],
+  //       text: "OS",
+  //       prefix: "/OS/",
+  //       children: "structure",
+  //     },
+  //     {
+  //       text: "ROS",
+  //       prefix: "/ROS/",
+  //       children: "structure",
   //     },
   //     {
   //       text: "security",
   //       prefix: "/security/",
-  //       children: [
-  //         "PE_file_format",
-  //       ],
-  //     },
+  //       children: "structure",
+  //     }
   //   ],
   // },
   // {
   //   text: "Cpp-Miner",
   //   prefix: "/Cpp-Miner/",
-  //   collapsible: false,
-  //   children: [
-  //     {
-  //       text: "新手教學",
-  //       prefix: "Miner_Tutorial/",
-  //       collapsible: false,
-  //       children: [
-  //         "Array_Pointer/",
-  //         "Environment_Building/",
-  //         "Object_Expression_Statement/",
-  //         "Class/",
-  //         "Function_Memory/",
-  //         "Computer_Introduction/",
-  //         "OO/",
-  //       ],
-        
-  //     },
-  //     {
-  //       text: "一些黑魔法",
-  //       prefix: "Miner_BlackMagic/",
-  //       children: "structure",
-  //     },
-  //     {
-  //       text: "礦坑系列文",
-  //       prefix: "Miner_main/",
-  //       children: "structure",
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "ROS",
-  //   prefix: "/ROS/",
   //   children: "structure",
   // },
   // {
-  //   text: "OS",
-  //   prefix: "/OS/",
+  //   text: "risc-v",
+  //   prefix: "/risc-v/",
+  //   children: "structure",
+  // },
+  // {
+  //   text: "Linux",
+  //   prefix: "/Linux/",
   //   children: "structure",
   // },
   // {
   //   text: "數值線代",
-  //   prefix: "/numerical_algebra/",
+  //   prefix: "/numerical_linear_algebra/",
   //   children: "structure",
   // },
   // {
-  //   text: "雜談",
+  //   text: "雜記",
   //   prefix: "/essay/",
   //   children: "structure",
   // },
